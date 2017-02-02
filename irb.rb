46d3861c96ec07560d68c8d26cc0e3b4366fce57
codeamac11 <codeamac11@gmail.com>
2017-02-01 20:24:10 -0600

    file consola IRB

---
+++ irb.rb
@@ -0,0 +1,33 @@
+irb(main):001:0> (4+4)
+=> 8
+irb(main):002:0> (6-4)
+=> 2
+irb(main):003:0> "el perrito" + " " + "es cafe"
+=> "el perrito es cafe"
+
+irb(main):001:0> a = "hola"
+=> "hola"
+irb(main):002:0> b "adios"
+irb(main):003:0> a + " " + b
+=> "hola adios"
+
+array_espués_date_cuenta_de_cual es la diferencia de imprimir a pantalla con_los_siguientes_métodosputs,print,p_einspect`.
+ 
+
+
+
+irb(main):001:0> def shout(word)
+irb(main):002:1> new_w = word.upcase!
+irb(main):003:1> end
+=> :shout
+irb(main):004:0> shout("perrito")
+=> "PERRITO"
+irb(main):005:0> shout("we")
+=> "WE"
+irb(main):006:0> shout("vamonos")
+=> "VAMONOS"
+irb(main):007:0> shout("hola")
+=> "HOLA"
+irb(main):008:0> 
+
+
